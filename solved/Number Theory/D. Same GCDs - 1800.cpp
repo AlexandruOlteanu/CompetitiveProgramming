566d8e27d7f362358a860ad39c2dbe70c749db46
AlexandruOlteanu <alexandruolteanu2001@gmail.com>
2024-02-01 12:53:07 +0200

    Solved 1800 problem

---
+++ solved/Number Theory/D. Same GCDs - 1800.cpp
@@ -0,0 +1,87 @@
+/*
+    Programmer : Alexandru Olteanu
+    Problem : https://codeforces.com/contest/1295/problem/D
+*/
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+template<typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+// GCC Optimizations
+// #pragma GCC optimize("Ofast")
+// #pragma GCC target("fma,sse,sse2,sse3,ssse3,sse4,popcnt")
+// #pragma GCC target("abm,mmx,avx,avx2,tune=native")
+// #pragma GCC optimize(3)
+// #pragma GCC optimize("inline")
+// #pragma GCC optimize("-fgcse")
+// #pragma GCC optimize("-fgcse-lm")
+// #pragma GCC optimize("-fipa-sra")
+// #pragma GCC optimize("-ftree-pre")
+// #pragma GCC optimize("-ftree-vrp")
+// #pragma GCC optimize("-fpeephole2")
+// #pragma GCC optimize("-ffast-math")
+// #pragma GCC optimize("-fsched-spec")
+// #pragma GCC optimize("unroll-loops")
+// Useful
+mt19937 rng((unsigned int) chrono::steady_clock::now().time_since_epoch().count());
+#define FastEverything  ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define HighPrecision cout << fixed << setprecision(17);
+typedef long long ll;
+typedef pair<int, int> pii;
+const int mod = 1000000007;
+const int mod2 = 100000000;
+const int md = 998244353LL;
+ll mypowr(ll a, ll b, ll mod1) {ll res = 1; if(b < 0)b = 0; a %= mod1; assert(b >= 0);
+for(; b; b >>= 1){if (b & 1) res = res * a % mod1;a = a * a % mod1;} return res;}
+ll mypow(ll a, ll b) {ll res = 1; if(b < 0)b = 0;assert(b >= 0);
+for(; b; b >>= 1){if(b & 1) res = res * a;a = a * a;} return res;}
+#define pb push_back
+#define fi first
+#define se second
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+
+// ifstream fin("input.txt");
+// ofstream fout("output.txt");
+// #define cin fin
+// #define cout fout
+
+const ll infll = 0x7fffffffffffffff;
+const int inf = 0x7fffffff;
+const int maxn = 2e5 + 5;
+
+// Algorithm for 1 number
+
+long long phi(long long n) {
+    long long res = n;
+    long long d = 2;
+    while (n != 1) {
+        if (n % d == 0) {
+            while (n % d == 0) n /= d;
+            res -= res / d;
+        }
+        if (d == 2) d = 3;
+        else d += 2;
+        if (d * d > n) d = n;
+    }
+
+    return res;
+}
+
+int main() {
+
+    FastEverything
+    HighPrecision
+    int test = 1;
+    cin>>test;
+    for (int tt = 1; tt <= test; ++tt) {
+
+        ll a, m;
+        cin >> a >> m;
+        cout << phi(m / __gcd(a, m)) << '\n';
+        
+    }
+
+    return 0;
+}
