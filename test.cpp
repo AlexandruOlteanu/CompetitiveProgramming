04f6f2cb17035a52f2f7e44f663955e4fb57aed4
AlexandruOlteanu <alexandruolteanu2001@gmail.com>
2025-01-05 10:15:26 +0200

    X

--- test.cpp
+++ test.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define FastEverything  ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+typedef long long ll;
 
 // ifstream fin("input.in");
 // ofstream fout("output.out");
