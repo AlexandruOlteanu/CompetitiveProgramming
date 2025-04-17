e43d8e747026060a7b2e0404d43f0567514a2df7
Alexandru Olteanu <alexandruolteanu2001@gmail.com>
2025-04-17 23:44:51 +0300

    X

--- main.cpp
+++ main.cpp
@@ -9,6 +9,19 @@ template<typename T>
 using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 // Useful
 mt19937 rng((unsigned int) chrono::steady_clock::now().time_since_epoch().count());
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
 #define LightningFastReadWrite ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #define HighPrecision cout << fixed << setprecision(17);
 typedef long long ll;
