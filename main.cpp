264f5a3026b194ed58d4db3764963b25088193c6
Alexandru Olteanu <alexandruolteanu2001@gmail.com>
2025-05-07 09:58:30 +0300

    X

--- main.cpp
+++ main.cpp
@@ -52,7 +52,7 @@ for(; b; b >>= 1){if(b & 1) res = res * a;a = a * a;} return res;}
 
 const long long infll = numeric_limits<long long> :: max();
 const int inf = numeric_limits<int> :: max();
-const int maxn = 2e5 + 5;
+const int maxn = 2e5 + 2;
 
 
 
