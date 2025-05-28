3d594e373ebabfea934707c40916eaa2965671cd
Alexandru Olteanu <alexandruolteanu2001@gmail.com>
2025-05-28 23:59:40 +0300

    X

---
+++ light.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+// ifstream fin("input.in");
+// ofstream fout("output.out");
+// #define cin fin
+// #define cout fout
+
+int main() {
+
+
+
+    return 0;
+}
