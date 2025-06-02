0b1356aaae37e754da1eaf4edf4f2cf3814ae14f
Alexandru Olteanu <alexandruolteanu2001@gmail.com>
2025-06-02 13:44:28 +0300

    X

--- main.cpp
+++ main.cpp
@@ -21,7 +21,7 @@ using namespace std;
 #ifdef Debug
     #include "debug.hpp"
     #define dbg(...) std::cerr << __DEBUG_UTIL__::outer << __LINE__ << ": [", __DEBUG_UTIL__::printer(#__VA_ARGS__, __VA_ARGS__)
-    #define debugArr(...) std::cerr << __DEBUG_UTIL__::outer << __LINE__ << ": [", __DEBUG_UTIL__::printerArr(#__VA_ARGS__, __VA_ARGS__)
+    #define dbgArr(...) std::cerr << __DEBUG_UTIL__::outer << __LINE__ << ": [", __DEBUG_UTIL__::printerArr(#__VA_ARGS__, __VA_ARGS__)
 #else
     #define dbg(...) ((void)0)
     #define dbgArr(...) ((void)0)
