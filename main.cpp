c0431233c312bb8a5e08dfc057f92c1dd727db4f
Alexandru Olteanu <alexandruolteanu2001@gmail.com>
2025-06-05 10:48:28 +0300

    X

--- main.cpp
+++ main.cpp
@@ -140,8 +140,10 @@ using ll  = long long;
 using pii = pair<int, int>;
 constexpr pair<int, int> dir4[] = {{-1, 0}, {0, -1}, {0, 1}, {1, 0}};
 constexpr pair<int, int> dir8[] = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
-constexpr long long infbig = numeric_limits<long long> :: max();
+constexpr long long infBig = numeric_limits<long long> :: max();
+constexpr long long infMid = 1e15;
 constexpr int inf = numeric_limits<int> :: max();
+constexpr int infSmall = 1e9;
 
 // ifstream fin("input.in");
 // ofstream fout("output.out");
