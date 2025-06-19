980dd914b0f193735f20d53edc32ce5e85550845
Alexandru Olteanu <alexandruolteanu2001@gmail.com>
2025-06-19 18:29:04 +0300

    X

--- template/Fenwick Tree/FenwickTree.cpp
+++ template/Fenwick Tree/FenwickTree.cpp
@@ -1,34 +1,42 @@
-	
+#include <algorithm>
 /*
-    Template created by Alexandru Olteanu {AlexandruINV}
+Template created by Alexandru Olteanu {AlexandruINV}
 */
 // https://github.com/AlexandruOlteanu/CompetitiveProgramming/blob/main/template/Fenwick%20Tree/FenwickTree.cpp
 template<typename A>
-struct FenwickTree{
+struct FenwickTree {
     vector<A> array;
     int n;
-    FenwickTree(int length){
-        array.resize(length + 1, 0);
-        n = length;
+
+    FenwickTree(int length) {
+        reset(length);
     }
- 
-    void modify(int x, ll val){
-        for(; x <= n; x += x & -x){
+
+    void modify(int x, ll val) {
+        for (; x <= n; x += x & -x) {
             array[x] += val;
         }
-        return;
     }
-    
-    ll calc(int x){
+
+    ll calc(int x) {
         ll val = 0;
-        for(; x > 0; x -= x & -x){
+        for (; x > 0; x -= x & -x) {
             val += array[x];
         }
         return val;
     }
-    
-    ll get(int l, int r){
+
+    ll get(int l, int r) {
         if (l > r) return 0;
-        return calc(r) - calc(l - 1);   
+        return calc(r) - calc(l - 1);
+    }
+
+    void reset() {
+        fill(array.begin(), array.end(), 0);
+    }
+
+    void reset(int new_n) {
+        n = new_n;
+        array.assign(n + 1, 0);
     }
 };
