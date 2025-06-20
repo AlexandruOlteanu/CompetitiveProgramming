b9c0b17649d4f3029d173a4aaad7a91a7d8b502e
Alexandru Olteanu <alexandruolteanu2001@gmail.com>
2025-06-20 23:03:17 +0300

    X

--- main.cpp
+++ main.cpp
@@ -37,45 +37,59 @@ constexpr int mod = 1000000007;
    ======================================================================== */
 // #define DebugMode
 // #define Generator
-#ifdef DebugMode
-    #include "debug/debug.hpp"
-    #define dbg(...) std::cerr << __DEBUG_UTIL__::outer << __LINE__ << \
-                     ": [", __DEBUG_UTIL__::printer(#__VA_ARGS__, __VA_ARGS__)
-    #define dbgArr(...) std::cerr << __DEBUG_UTIL__::outer << __LINE__ << \
-                     ": [", __DEBUG_UTIL__::printerArr(#__VA_ARGS__, __VA_ARGS__)
-#else
-    #define dbg(...) ((void)0)
-    #define dbgArr(...) ((void)0)
-#endif
+namespace Debug {
+    #ifdef DebugMode
+        #include "debug/debug.hpp"
+        #define dbg(...) std::cerr << __DEBUG_UTIL__::outer << __LINE__ << \
+                    ": [", __DEBUG_UTIL__::printer(#__VA_ARGS__, __VA_ARGS__)
+        #define dbgArr(...) std::cerr << __DEBUG_UTIL__::outer << __LINE__ << \
+                    ": [", __DEBUG_UTIL__::printerArr(#__VA_ARGS__, __VA_ARGS__)
+    #else
+        #define dbg(...) ((void)0)
+        #define dbgArr(...) ((void)0)
+    #endif
+}
+using namespace Debug;
 
 /* ========================================================================
-   -> Policy‑based ordered set shortcut
+   -> Policy‑based ordered set and multiset
    ======================================================================== */
-template<typename T>
-using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+namespace OrderedStructures {
+    using namespace __gnu_pbds;
+
+    template<class T, class C = std::less<T>>
+    using ordered_set = tree<T, null_type, C, rb_tree_tag, tree_order_statistics_node_update>;
+
+    template<class T, class C = std::less<T>>
+    using ordered_multiset = tree<std::pair<T, int>, null_type, C, rb_tree_tag, tree_order_statistics_node_update>;
+}
+using namespace OrderedStructures;
 
 /* ========================================================================
    -> Custom hash for unordered containers
    ======================================================================== */
-struct numberHash {
-    static uint64_t splitmix64(uint64_t x) {
-        x += 0x9e3779b97f4a7c15ULL;
-        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9ULL;
-        x = (x ^ (x >> 27)) * 0x94d049bb133111ebULL;
-        return x ^ (x >> 31);
-    }
-    size_t operator()(uint64_t x) const {
-        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
-        return splitmix64(x + FIXED_RANDOM);
-    }
-};
+namespace Hash {
+    struct numberHash {
+        static uint64_t splitmix64(uint64_t x) {
+            x += 0x9e3779b97f4a7c15ULL;
+            x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9ULL;
+            x = (x ^ (x >> 27)) * 0x94d049bb133111ebULL;
+            return x ^ (x >> 31);
+        }
+        size_t operator()(uint64_t x) const {
+            static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+            return splitmix64(x + FIXED_RANDOM);
+        }
+    };
 
-template<typename T, typename U>
-struct pairHash {
-    size_t operator()(const pair<T, U>& p) const {
-        return numberHash{}(p.first ^ (numberHash{}(p.second) << 1));
-    }
-};
+    template<typename T, typename U>
+    struct pairHash {
+        size_t operator()(const pair<T, U>& p) const {
+            return numberHash{}(p.first ^ (numberHash{}(p.second) << 1));
+        }
+    };
+}
+using namespace Hash;
 
 /* ========================================================================
    -> Functions
@@ -258,33 +272,33 @@ using namespace Functions;
 /* ========================================================================
    -> Defines
    ======================================================================== */
-#define all(x) (x).begin(), (x).end()
-#define all1(x) ++(x).begin(), (x).end()
-#define rall(x) (x).rbegin(), (x).rend()
-#define rall1(x) (x).rbegin(), --(x).rend()
-using ll  = long long;
-using pii = pair<int, int>;
-constexpr pair<int, int> dir4[] = {{-1, 0}, {0, -1}, {0, 1}, {1, 0}};
-constexpr pair<int, int> dir8[] = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
-constexpr pair<int, int> dirDiag[] = {{-1, -1}, {-1, 1}, {1, -1}, {1, 1}};
-constexpr long long infBig = numeric_limits<long long> :: max() - 23;
-constexpr long long infMid = 1e15;
-constexpr int inf = numeric_limits<int> :: max() - 23;
-constexpr int infSmall = 1e9;
+namespace Define {
+    #define all(x) (x).begin(), (x).end()
+    #define all1(x) ++(x).begin(), (x).end()
+    #define rall(x) (x).rbegin(), (x).rend()
+    #define rall1(x) (x).rbegin(), --(x).rend()
+    using ll  = long long;
+    using pii = pair<int, int>;
+    constexpr pair<int, int> dir4[] = {{-1, 0}, {0, -1}, {0, 1}, {1, 0}};
+    constexpr pair<int, int> dir8[] = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
+    constexpr pair<int, int> dirDiag[] = {{-1, -1}, {-1, 1}, {1, -1}, {1, 1}};
+    constexpr long long infBig = numeric_limits<long long> :: max() - 23;
+    constexpr long long infMid = 1e15;
+    constexpr int inf = numeric_limits<int> :: max() - 23;
+    constexpr int infSmall = 1e9;
+}
+using namespace Define;
 
-/* ========================================================================
-   -> Templates Start
-   ======================================================================== */
+namespace Template {
 
-/* ========================================================================
-   -> Templates End
-   ======================================================================== */
+}
+using namespace Template;
 
 constexpr int MAXN = 2e5 + 2;
 
 
 
-void CoreCompute(const int testIdx, const bool isLastTest) {
+void Compute(const int testId, const bool isLastTest) {
 
     
 
@@ -304,31 +318,29 @@ int main() {
     #endif
 
     #if defined(DebugMode) && !defined(Generator)
+        auto getDuration = [] (auto start, auto end) {return chrono::duration<double>(end - start).count();};
         const auto startPrecomputation = chrono::high_resolution_clock::now();
     #endif
 
     Precompute();
 
     #if defined(DebugMode) && !defined(Generator)
-        const auto endPrecompute = chrono::high_resolution_clock::now();
-        const chrono::duration<double> precomputationDuration = endPrecompute - startPrecomputation;
-        const auto startCoreComputation = chrono::high_resolution_clock::now();
+        const auto endPrecomputation = chrono::high_resolution_clock::now();
+        const auto startComputation = endPrecomputation;
     #endif
 
-    for (int testIdx = 1; testIdx <= numberOfTests; ++testIdx) {
-        // cout << "Case #" << testIdx << ": ";
-        // cout << "Test #" << testIdx << ": ";
-        CoreCompute(testIdx, testIdx == numberOfTests);
+    for (int testId = 1; testId <= numberOfTests; ++testId) {
+        // cout << "Case #" << testId << ": ";
+        // cout << "Test #" << testId << ": ";
+        Compute(testId, testId == numberOfTests);
     }
 
     #if defined(DebugMode) && !defined(Generator)
+        const auto endComputation = chrono::high_resolution_clock::now();
         cout << "\n\n=================================================================\n";
-        cout << "-> Precomputation Running Time:    " << precomputationDuration.count() << " seconds." << '\n';
-        const auto endCoreComputation = chrono::high_resolution_clock::now();
-        const chrono::duration<double> coreComputationDuration = endCoreComputation - startCoreComputation;
-        cout << "-> Core Computation Running Time:  " << coreComputationDuration.count() << " seconds." << "\n";
-        const chrono::duration<double> totalComputationDuration = endCoreComputation - startPrecomputation;
-        cout << "-> Total Computation Running Time: " << totalComputationDuration.count() << " seconds." << "\n";
+        cout << "-> Precomputation Running Time:    " << getDuration(startPrecomputation, endPrecomputation) << " seconds." << '\n';
+        cout << "-> Computation Running Time:       " << getDuration(startComputation, endComputation) << " seconds." << "\n";
+        cout << "-> Total Computation Running Time: " << getDuration(startPrecomputation, endComputation) << " seconds." << "\n";
         cout << "=================================================================\n";
     #endif
 
