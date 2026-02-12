d0afb1d4e229d4097dd7b697e3de28207b364617
Alexandru Olteanu <alexandruolteanu2001@gmail.com>
2026-02-12 22:45:57 +0200

    X

---
+++ professional_short.cpp
@@ -0,0 +1,46 @@
+/* =====================================================================================================
+
+     ██████╗ ██╗   ██╗ ███╗  ██╗      ██╗ ███████╗  █████╗  ████████╗  █████╗  ██████╗ 
+    ██╔════╝ ╚██╗ ██╔╝ ████╗ ██║      ██║ ██╔════╝ ██╔══██╗ ╚══██╔══╝ ██╔══██╗ ██╔══██╗
+    ╚█████╗   ╚████╔╝  ██╔██╗██║      ██║ █████╗   ██║  ╚═╝    ██║    ██║  ██║ ██████╔╝
+     ╚═══██╗   ╚██╔╝   ██║╚████║ ██╗  ██║ ██╔══╝   ██║  ██╗    ██║    ██║  ██║ ██╔══██╗
+    ██████╔╝    ██║    ██║ ╚███║ ╚█████╔╝ ███████╗ ╚█████╔╝    ██║    ╚█████╔╝ ██║  ██║
+    ╚═════╝     ╚═╝    ╚═╝  ╚══╝  ╚════╝  ╚══════╝  ╚════╝     ╚═╝     ╚════╝  ╚═╝  ╚═╝
+
+   ===================================================================================================== */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+constexpr int mod = 1000000007;
+
+// ifstream fin("input.in");
+// ofstream fout("output.out");
+// #define cin fin
+// #define cout fout
+
+using ll  = long long;
+using pii = pair<int, int>;
+
+void Inject() {
+    
+    
+
+}
+
+constexpr bool activateMultipleTests = false;
+int main() {
+
+    ios_base::sync_with_stdio(false); cin.tie(nullptr);
+
+    int numberOfTests = 1;
+    if (activateMultipleTests) {
+        cin >> numberOfTests;
+    }
+
+    for (int testNr = 1; testNr <= numberOfTests; ++testNr) {
+        Inject();
+    }
+
+    return 0;
+}
