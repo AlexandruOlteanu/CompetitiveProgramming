c3ad2a185b9d7fc4f0409db1bfa78698fc8d2e26
Alexandru Olteanu <alexandruolteanu2001@gmail.com>
2026-02-16 17:37:07 +0200

    X

--- professional.cpp
+++ professional.cpp
@@ -10,7 +10,7 @@
     Injected:
     ->   13  Official Championships Problems
     ->   0   Hard Problems (Aimed to improve my skills and deep thinking)
-    ->   4   Leveling Up Problems (A mix of speed and thinking, climbing the Level AK ladder)
+    ->   8   Leveling Up Problems (A mix of speed and thinking, climbing the Level AK ladder)
     ->   0   Virtual Contests Problems (Aimed to simulate live performances)
     ->   10  Random Problems from different sites
    ===================================================================================================== */
